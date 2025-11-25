6b55970c37ca6bf5d5e5e40c4872a2ebb6f47193
Ryan Ly <310197+rly@users.noreply.github.com>
2025-11-25 13:56:54 -0800

    Add type hints and docstrings, rm session_info==none case

--- src/mindscope_to_nwb_zarr/aind_data_schema/utils.py
+++ src/mindscope_to_nwb_zarr/aind_data_schema/utils.py
@@ -1,30 +1,34 @@
 
 from datetime import datetime, timezone
+import pandas as pd
+from pynwb import NWBFile
 import warnings
 
-def get_subject_id(nwbfile, session_info=None):
-    if session_info is not None:
-        assert session_info['mouse_id'].values[0] == int(nwbfile.subject.subject_id), "subject_id mismatch occurred"
+def get_subject_id(nwbfile: NWBFile, session_info: pd.DataFrame) -> str:
+    """Get the subject ID from the NWB file, cross-checked with the session info. e.g., "457841"."""
+    assert session_info['mouse_id'].values[0] == int(nwbfile.subject.subject_id), "subject_id mismatch occurred"
     return nwbfile.subject.subject_id
 
-def get_session_start_time(nwbfile, session_info=None):
-    if session_info is not None:
-        session_time = datetime.fromisoformat(session_info['date_of_acquisition'].values[0])
-        session_time_utc = session_time.astimezone(timezone.utc).replace(microsecond=0)
-        nwb_time_utc = nwbfile.session_start_time.astimezone(timezone.utc).replace(microsecond=0)   
+def get_session_start_time(nwbfile: NWBFile, session_info: pd.DataFrame) -> datetime:
+    """Get the session start time from the NWB file, cross-checked with the session info. 
+    e.g., datetime object for 2018-08-24T14:51:25.667000+00:00
+    """
+    session_time = datetime.fromisoformat(session_info['date_of_acquisition'].values[0])
+    session_time_utc = session_time.astimezone(timezone.utc).replace(microsecond=0)
+    nwb_time_utc = nwbfile.session_start_time.astimezone(timezone.utc).replace(microsecond=0)   
 
-        if session_time_utc != nwb_time_utc:
-            warnings.warn(
-                f"session_start_time mismatch - using nwbfile value. "
-                f"session_info={session_time_utc}, nwbfile={nwb_time_utc}"
-            )
+    if session_time_utc != nwb_time_utc:
+        warnings.warn(
+            f"session_start_time mismatch - using nwbfile value. "
+            f"session_info={session_time_utc}, nwbfile={nwb_time_utc}"
+        )
 
     return nwbfile.session_start_time
 
-def get_instrument_id(nwbfile, session_info=None):
+def get_instrument_id(nwbfile: NWBFile, session_info) -> str:
+    """Get the instrument ID from the NWB file, cross-checked with the session info. e.g. "BEH.F-Box1"."""
     instrument = next(iter(nwbfile.devices))
-    if session_info is not None:
-        assert session_info['equipment_name'].values[0] == instrument, "instrument_id mismatch occurred"
+    assert session_info['equipment_name'].values[0] == instrument, "instrument_id mismatch occurred"
     return instrument
 
 def get_total_reward_volume(nwbfile):
