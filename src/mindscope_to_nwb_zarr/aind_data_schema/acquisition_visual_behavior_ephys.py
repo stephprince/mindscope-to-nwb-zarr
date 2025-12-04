a1f3fd0f83cf5b0b4d9636c31a912bf2b0d47248
Steph Prince <40640337+stephprince@users.noreply.github.com>
2025-12-03 17:19:05 -0800

    update todo items with notes

--- src/mindscope_to_nwb_zarr/aind_data_schema/acquisition_visual_behavior_ephys.py
+++ src/mindscope_to_nwb_zarr/aind_data_schema/acquisition_visual_behavior_ephys.py
@@ -51,11 +51,8 @@ from mindscope_to_nwb_zarr.aind_data_schema.utils import (
 
 # example file for initial debugging
 # TODO - replace with more general ingestion/generation script
-# TODO - test with pure behavior files
-# TODO - add performance metrics for behavior if available
-# TODO - fill in additional missing sections
 
-behavior_only = True # set to True to test with behavior only session
+behavior_only = False # set to True to test with behavior only session
 repo_root = Path(__file__).parent.parent.parent.parent
 cache_dir = repo_root / ".cache/visual_behavior_neuropixels_cache_dir/visual-behavior-neuropixels-0.5.0/project_metadata/"
 
@@ -65,8 +62,8 @@ if behavior_only:
     nwbfile = read_nwb(repo_root / f"data/behavior_session_{session_id}.nwb")
 else:
     session_id = 1043752325
-    # nwbfile = read_nwb(repo_root / f"data/sub-{subject_id}_ses-20200817T222149.nwb")
-    nwbfile = read_nwb(repo_root / f"data/ecephys_session_{session_id}.nwb")
+    nwbfile = read_nwb(repo_root / f"data/sub-{subject_id}_ses-20200817T222149.nwb")
+    # nwbfile = read_nwb(repo_root / f"data/ecephys_session_{session_id}.nwb")
 
 # load metadata files
 ephys_session_table = pd.read_csv(cache_dir / "ecephys_sessions.csv")
@@ -79,27 +76,36 @@ if len(session_info) == 0 and len(behavior_session_info) == 1:
 
 def get_probe_configs(nwbfile):
     probe_configs = []
+    all_targeted_structures = []
     for device in nwbfile.devices.values():
         if device.__class__.__name__ == "EcephysProbe":
             locations = (nwbfile.electrodes.to_dataframe()
                          .query('group_name == @device.name')['location'].unique().tolist())
-            targeted_structures = [getattr(CCFv3, l.upper()) for l in locations if getattr(CCFv3, l.upper(), None) is not None]
-            assert len(targeted_structures) == len(locations), "All probe locations not found in CCFv3 enum"
+            all_structures = [getattr(CCFv3, l.upper()) for l in locations if getattr(CCFv3, l.upper(), None) is not None]
+            targeted_structure = [s for s in all_structures if s.acronym.startswith('VIS')] # get targeted visual area
+            assert len(all_structures) == len(locations), "All probe locations not found in CCFv3 enum"
+            assert len(targeted_structure) == 1, "More than one visual area found"
+            all_targeted_structures.append(targeted_structure[0])
 
             probe_configs.append(
                 ProbeConfig(
                     device_name=device.name,
-                    primary_targeted_structure=CCFv3.VIS, # TODO - update if need to be more specific
-                    other_targeted_structure=targeted_structures,
+                    # 6 probes, each targets a cortical visual area (e.g. VISp, VISl, VISal, VISrl, VISam, VISpm)
+                    # would list that specific area as the primary targeted structure 
+                    # should be the same for every experiment, most files should have majority of one
+                    primary_targeted_structure=targeted_structure[0],
+                    other_targeted_structure=list(set(all_structures) - set(targeted_structure)), # TODO - currently listing all other structures that are hit but might want to not list everything
                     atlas_coordinate=AtlasCoordinate(
                         coordinate_system=AtlasLibrary.CCFv3_10um,
-                        translation=[0, 0, 0], # TODO - should be target region coordinate
+                        translation=[0, 0, 0], # TODO - should be target region coordinate - might not make sense for these datasets, TBD @Saskia
                     ),
-                    coordinate_system=CoordinateSystemLibrary.MPM_MANIP_RFB, # TODO - what should this be?
-                    transform=[Translation(translation=[0, 0, 0, 1],),], # TODO - what should this be?
+                    coordinate_system=CoordinateSystemLibrary.MPM_MANIP_RFB, # TODO - what should this be? probably bregma ARID, will confirm
+                    transform=[Translation(translation=[0, 0, 0, 1],),], # TODO - what should this be? this will be the translation we care about, how we've positioned this probe
+                    # expect that there is documentation on these translations somewhere @Saskia
                     notes=None,
                 )
             )
+    assert len(set(all_targeted_structures)) == len(all_targeted_structures), "Duplicate targeted structures found across probes"
     
     return probe_configs
 
@@ -111,25 +117,26 @@ def get_optostimulation_parameters(optogenetic_stimulation):
         if 'square' in df['condition'].values[0]:
             pulse_shape = PulseShape.SQUARE
         elif 'cosine' in df['condition'].values[0]:
-            pulse_shape = PulseShape.SINE # TODO - also described as "raised cosine ramp" in whitepaper
+            pulse_shape = PulseShape.SINE # TODO - also described as "raised cosine ramp" in whitepaper, may need to define new enum
         
         # convert mean intervals to frequency
         pulse_frequency = 1.0 / np.mean(np.diff(df['start_time'])) 
 
         opto_stimulation[stimulus_name] = (
+            # TODO - replace this with a new stimulation class inspired by this one with appropriate set of parameters / fields
             OptoStimulation(
                 stimulus_name=stimulus_name,
                 pulse_shape=pulse_shape,
-                pulse_frequency=[pulse_frequency], # TODO - what should this be?
+                pulse_frequency=[pulse_frequency], # TODO - remove in custom class
                 pulse_frequency_unit=FrequencyUnit.HZ,
-                number_pulse_trains=[len(df)], # TODO- what should this be?
+                number_pulse_trains=[len(df)], # TODO - remove in custom class
                 pulse_width=(df['duration'].unique() * 100).astype(int).tolist(),
                 pulse_width_unit=TimeUnit.MS,
-                pulse_train_duration=(df['duration'].unique() * 100).astype(int).tolist(), # TODO - what should this be?
+                pulse_train_duration=(df['duration'].unique() * 100).astype(int).tolist(), # TODO - remove in custom class
                 pulse_train_interval=1.5, # from technical whitepaper
                 fixed_pulse_train_interval=False, # from technical whitepaper
                 pulse_train_interval_unit=TimeUnit.S,
-                baseline_duration=0.0, # TODO - is whole prior recording considered baseline? add if needed
+                baseline_duration=0.0, # TODO - remove in custom class
                 baseline_duration_unit=TimeUnit.S,
                 notes=f"{df['condition'].values[0]} with three light levels: {df['level'].unique().tolist()}",
             )
@@ -139,7 +146,7 @@ def get_optostimulation_parameters(optogenetic_stimulation):
 
 def get_visual_stimulation_parameters(table_key: str, intervals_table: pd.DataFrame) -> VisualStimulation:
     # TODO - determine if there are any other parameters to include
-    # NOTE - parameter serialization to JSON is not showing up correctly, also an issue in the example file
+    # NOTE - parameter serialization to JSON is not showing up correctly, also an issue in the example file, submit an issue to fix
     possible_parameters_and_units = {"orientation": "degrees",
                                      "spatial_frequency": "cycles/degree",
                                      "temporal_frequency": "Hz",
@@ -168,18 +175,14 @@ def convert_intervals_to_stimulus_epochs(stimulus_name: str, table_key: str, int
                 stimulus_start_time=timedelta(seconds=intervals_table['start_time'].values[0]) + nwbfile.session_start_time,
                 stimulus_end_time=timedelta(seconds=intervals_table['stop_time'].values[-1]) + nwbfile.session_start_time,
                 stimulus_name=stimulus_name,
-                code=Code( # TODO - acquire additional info about the code used for this task
+                # TODO - acquire additional info about the code used for this task - might not be available 
+                # will need to fill in with some type of information so we can use the Code.parameters field @Saskia
+                code=Code(
                     url="None",
-                    name="None",
-                    version="None",
-                    container=None,
-                    run_script=None,
-                    language=None,
-                    language_version=None,
-                    input_data=None,
                     core_dependency=Software(
                         name="PsychoPy",
-                        version=None,), # TODO - add software version if available
+                        version=None,
+                    ), # TODO - from whitepaper, add version if available @Saskia
                     parameters=get_visual_stimulation_parameters(table_key, intervals_table),
                 ),
                 stimulus_modalities=[StimulusModality.VISUAL],
@@ -193,22 +196,22 @@ def convert_intervals_to_stimulus_epochs(stimulus_name: str, table_key: str, int
 def get_stimulation_epochs(nwbfile):
     # loop through all intervals tables
     stimulation_epochs = []
-    stimulation_type = ["Gabors", "Spontaneous", "Passive replay", "Full-field flashes", "Grating"] # TODO - determine if any other types to consider
+    stimulation_type = ["Gabor", "Spontaneous", "Passive replay", "Flash", "Grating"] # TODO - determine if any other types to consider
     for table_key, intervals_table in nwbfile.intervals.items():
         # skip generic trials table that contains behavioral data
         if table_key == "trials":
             continue
         # split active and passive behavior sessions into different stimulus epochs
         elif table_key == "Natural_Images_Lum_Matched_set_ophys_G_2019_presentations":
-            active_intervals = intervals_table.to_dataframe()['active'].query('active == True')
-            stimulus_name = "Active behavior"
+            active_intervals = intervals_table.to_dataframe().query('active == True')
+            stimulus_name = "Change detection - Active"
             stim_epoch = convert_intervals_to_stimulus_epochs(stimulus_name=stimulus_name, 
                                                             table_key=table_key, 
                                                             intervals_table=active_intervals)
             stimulation_epochs.append(stim_epoch)
 
-            passive_intervals = intervals_table.to_dataframe()['active'].query('active == False')
-            stimulus_name = "Passive replay"
+            passive_intervals = intervals_table.to_dataframe().query('active == False')
+            stimulus_name = "Change detection - Passive replay"
             stim_epoch = convert_intervals_to_stimulus_epochs(stimulus_name=stimulus_name, 
                                                             table_key=table_key, 
                                                             intervals_table=passive_intervals)
@@ -232,16 +235,6 @@ def get_stimulation_epochs(nwbfile):
             stimulus_name="Optotagging",
             code=Code( # TODO - add code source if available
                 url="None",
-                name="None",
-                version="None",
-                container=None,
-                run_script=None,
-                language=None,
-                language_version=None,
-                input_data=None,
-                core_dependency=Software(
-                    name="None",
-                    version=None,), # TODO - add software if available
                 parameters=get_optostimulation_parameters(optogenetic_stimulation),
             ),
             stimulus_modalities=[StimulusModality.OPTOGENETICS],
@@ -263,25 +256,27 @@ def get_stimulation_epochs(nwbfile):
 
 acquisition = Acquisition(
     subject_id=get_subject_id(nwbfile, session_info=session_info),
-    specimen_id=None, # TODO - confirm whether necessary for in vivo ephys files
     acquisition_start_time=get_session_start_time(nwbfile, session_info=session_info),
     acquisition_end_time=get_data_stream_end_time(nwbfile),
-    protocol_id=None, # TODO - confirm whether shared on protocols.io
-    ethics_review_id=None, #TODO - obtain if available
+    ethics_review_id=None, #TODO - obtain if available - YES, @Saskia
     instrument_id=get_instrument_id(nwbfile, session_info=session_info),
-    acquisition_type=nwbfile.session_description, # TODO - confirm consistent across experiments or if better option
+    acquisition_type=nwbfile.session_description, # TODO - pull from session_type through allensdk (e..g 3sessionA, 3sessionB, BrainObservatory1.1)
     notes=None,
-    coordinate_system=CoordinateSystemLibrary.BREGMA_ARID, # TODO - determine correct coordinate system library
-    # calibrations=[], # TODO - add if available
+    coordinate_system=CoordinateSystemLibrary.BREGMA_ARID, # TODO - determine correct coordinate system library, will also be defined with instrument (not required to be same as acquisition)
+    # coordinate system info might not be available, will check @Saskia
+    # calibrations=[], # TODO - add if available - will be difficult to find, probably not
     # maintenance=[],
-    data_streams=[ # TODO - confirm single stream of ephys + behavior data is appropriate
+    data_streams=[
         DataStream(
             stream_start_time=get_data_stream_start_time(nwbfile),
             stream_end_time=get_data_stream_end_time(nwbfile),
             modalities=get_modalities(nwbfile),
             code=None,
             notes=None,
-            active_devices=[ # TODO - determine all active devices names that would apply and their names
+            # active devices will be placeholders depending on the instrument information getting filled in
+            # configurations will also be dependent on instrument information 
+            # TODO - wait for instrument information but could maybe get some placeholders for active device names @Saskia
+            active_devices=[
                 "EPHYS_1", # TODO - add conditional for behavioral data to select appropriate devices
                 "Laser_1",
                 "Lick_Spout_1",
@@ -291,23 +286,23 @@ acquisition = Acquisition(
                     device_name="EPHYS_1",
                     manipulator=ManipulatorConfig(
                         device_name="Manipulator_1", # TODO - fill in with correct information
-                        coordinate_system=CoordinateSystemLibrary.MPM_MANIP_RFB,
-                        local_axis_positions=Translation(translation=[0, 0, 0],), # TODO - fill in with correct positions
+                        coordinate_system=CoordinateSystemLibrary.MPM_MANIP_RFB, # should be standardized (confirm relative to bregma, positions) @Saskia
+                        local_axis_positions=Translation(translation=[0, 0, 0],), # TODO - fill in with correct positions @Saskia
                     ),
                     probes=get_probe_configs(nwbfile),
                 ),
                 LaserConfig( # TODO - should this go here or in the stimulation epochs configuration field?
-                    device_name="Laser_1",
+                    device_name="Laser_1", # placeholder
                     wavelength=473, # from technical whitepaper
                     wavelength_unit=SizeUnit.NM,
                 ),
                 LickSpoutConfig(
-                    device_name="Lick_Spout_1",
+                    device_name="Lick_Spout_1", # placeholder
                     solution=Liquid.WATER,
                     solution_valence=Valence.POSITIVE,
                     volume=get_individual_reward_volume(nwbfile),
                     volume_unit=VolumeUnit.ML,
-                    relative_position=["Anterior"], # TODO - what is the correct information here?
+                    relative_position=["Anterior"], # TODO - what is the correct information here? 
                 )
                 # TODO - add information about Monitor, Camera, LED from nwbfile.processing['eye_tracking_rig_metadata']['eye_tracking_rig_metadata']
                 # TODO - should we add MousePlatformConfig here too?
@@ -316,12 +311,12 @@ acquisition = Acquisition(
     ],
     stimulus_epochs=get_stimulation_epochs(nwbfile),
     subject_details=AcquisitionSubjectDetails(
-        animal_weight_prior=None, # TODO - pull in extra info if available
+        animal_weight_prior=None, # TODO - pull in extra info if available - likely not available @Saskia
         animal_weight_post=None,
         weight_unit=MassUnit.G,
         anaesthesia=None,
         mouse_platform_name="Running Wheel",
-        reward_consumed_total=get_total_reward_volume(nwbfile), # TODO - check if calculation is ok
+        reward_consumed_total=get_total_reward_volume(nwbfile),
         reward_consumed_unit=VolumeUnit.ML
     ),
 )
