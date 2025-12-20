5c53c8c5236de3993823b6b66a92c55505b494e6
Steph Prince <40640337+stephprince@users.noreply.github.com>
2025-12-19 17:38:47 -0800

    update mouse id script

--- code/scripts/get_mouse_ids_from_allensdk.py
+++ code/scripts/get_mouse_ids_from_allensdk.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 
 from pathlib import Path
 
@@ -13,31 +14,31 @@ mouse_ids = dict()
 
 # download Visual Behavior - Neuropixels data to local cache
 output_dir =  Path(".cache/visual_behavior_neuropixels_cache_dir")
-cache = VisualBehaviorNeuropixelsProjectCache.from_s3_cache(cache_dir=output_dir)
+visual_behavior_neuropixels_cache = VisualBehaviorNeuropixelsProjectCache.from_s3_cache(cache_dir=output_dir)
 
-ephys_session_table = cache.get_ecephys_session_table()
-behavior_session_table = cache.get_behavior_session_table()
+vis_behavior_neuropixels_ephys_session_table = visual_behavior_neuropixels_cache.get_ecephys_session_table()
+vis_behavior_neuropixels_session_table = visual_behavior_neuropixels_cache.get_behavior_session_table()
 
-mouse_ids_behavior = behavior_session_table['mouse_id'].unique().tolist()
-mouse_ids_ephys = ephys_session_table['mouse_id'].unique().tolist()
-mouse_ids['visual_behavior_ephys'] = list(set(mouse_ids_behavior) | set(mouse_ids_ephys))
+mouse_ids_vis_behavior_neuropixels = vis_behavior_neuropixels_session_table['mouse_id'].unique().tolist()
+mouse_ids_vis_behavior_neuropixels_ephys = vis_behavior_neuropixels_ephys_session_table['mouse_id'].unique().tolist()
+mouse_ids['visual_behavior_ephys'] = list(set(mouse_ids_vis_behavior_neuropixels) | set(mouse_ids_vis_behavior_neuropixels_ephys))
 
 # dwnload Visual Behavior - Optophysiology data to local cache
 output_dir =  Path(".cache/visual_behavior_ophys_cache_dir")
-cache = VisualBehaviorOphysProjectCache.from_s3_cache(cache_dir=output_dir)
+visual_behavior_ophys_cache = VisualBehaviorOphysProjectCache.from_s3_cache(cache_dir=output_dir)
 
-ephys_session_table = cache.get_ophys_session_table()
-behavior_session_table = cache.get_behavior_session_table()
+vis_behavior_ophys_session_table = visual_behavior_ophys_cache.get_ophys_session_table()
+behavior_session_table = visual_behavior_ophys_cache.get_behavior_session_table()
 
 # pull all unique mouse ids from the behavior sessions file
-mouse_ids_behavior = behavior_session_table['mouse_id'].unique().astype(int).tolist()
-mouse_ids_ephys = ephys_session_table['mouse_id'].unique().astype(int).tolist()
-mouse_ids['visual_behavior_ophys'] = list(set(mouse_ids_behavior) | set(mouse_ids_ephys))
+mouse_ids_ophys_behavior = behavior_session_table['mouse_id'].unique().astype(int).tolist()
+mouse_ids_ophys = behavior_session_table['mouse_id'].unique().astype(int).tolist()
+mouse_ids['visual_behavior_ophys'] = list(set(mouse_ids_ophys_behavior) | set(mouse_ids_ophys))
 
 # Download Visual Coding - Neuropixels data to local cache
 output_dir =  Path(".cache/visual_coding_ephys_cache_dir")
-cache = EcephysProjectCache.from_warehouse(manifest=str(Path(output_dir) / 'visual_coding_ephys_manifest.json'))
-coding_ephys_sessions = cache.get_session_table()
+visual_coding_ephys_cache = EcephysProjectCache.from_warehouse(manifest=str(Path(output_dir) / 'visual_coding_ephys_manifest.json'))
+coding_ephys_sessions = visual_coding_ephys_cache.get_session_table()
 
 # pull all unique mouse ids from the behavior sessions file
 full_session_table = build_and_execute(
@@ -49,29 +50,21 @@ full_session_table = build_and_execute(
         ",rma::include,specimen(donor(age))"
     ),
     base=rma_macros(),
-    engine=cache.fetch_api.rma_engine.get_rma_tabular,
+    engine=visual_coding_ephys_cache.fetch_api.rma_engine.get_rma_tabular,
     session_ids=None,
 )
-donor_ids = set([s['donor_id'] for s in full_session_table['specimen'].tolist()])  # These match subject IDs on DANDI
-specimen_ids = coding_ephys_sessions['specimen_id'].unique().tolist() 
-external_specimen_ids = set([s['external_specimen_name'] for s in full_session_table['specimen'].tolist()])  # These match subject IDs on DANDI
+# donor_ids = set([s['donor_id'] for s in full_session_table['specimen'].tolist()])  # These match subject IDs on DANDI but are not used
+external_specimen_ids = set([int(s['external_specimen_name']) for s in full_session_table['specimen'].tolist()])
 mouse_ids['visual_coding_ephys'] = external_specimen_ids
-# TODO - external_specimen_name here seems to be 6 digits
 
 # Download Visual Coding - Optophysiology data to local cache
 output_dir =  Path(".cache/visual_coding_ophys_cache_dir")
-cache = BrainObservatoryCache(manifest_file=str(Path(output_dir) / 'visual_coding_ophys_manifest.json'))
-sessions = cache.get_ophys_experiments()
-experiment_info = cache.get_experiment_containers()
-with open(Path(output_dir) / 'experiment_containers.json', 'r') as f:
-    experiment_containers = json.load(f)
+visual_coding_ophys_cache = BrainObservatoryCache(manifest_file=str(Path(output_dir) / 'visual_coding_ophys_manifest.json'))
+sessions_df = pd.DataFrame(visual_coding_ophys_cache.get_ophys_experiments(include_failed=True))
+sessions_df = sessions_df.query('id <= 717913184') # no session ids greater than this on quilt3 bucket / DANDI
 
-specimen_ids = set(list({e["specimen_id"] for e in experiment_containers})) # These match the subject IDs on DANDI
-donor_ids = set(list({e["specimen"]["donor_id"] for e in experiment_containers})) 
-specimen_ids_filtered = [s for s in specimen_ids if s <= 699502603] # dandiset and S3 bucket only includes data from these mouse ids
-donor_name_ids = set([s["donor_name"] for s in sessions])
+donor_name_ids = sessions_df['donor_name'].unique().astype(int).tolist()
 mouse_ids['visual_coding_ophys'] = donor_name_ids
-# TODO - donor name here seems to be correct
 
 # save output data
 mouse_ids_serializable = {k: sorted(list(v)) if isinstance(v, set) else sorted(v)
