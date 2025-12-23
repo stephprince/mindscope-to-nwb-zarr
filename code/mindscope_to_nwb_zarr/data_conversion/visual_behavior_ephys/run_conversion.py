6e87835e211aeb8a77ed28a0410245b767c57280
Steph Prince <40640337+stephprince@users.noreply.github.com>
2025-12-22 22:01:24 -0500

    fix path handling and potential resource leaks

--- code/mindscope_to_nwb_zarr/data_conversion/visual_behavior_ephys/run_conversion.py
+++ code/mindscope_to_nwb_zarr/data_conversion/visual_behavior_ephys/run_conversion.py
@@ -31,23 +31,24 @@ def convert_visual_behavior_ephys_file_to_zarr(hdf5_base_filename: Path, zarr_fi
 
         # pull additional data from each of the probe files and add to the main nwbfile
         io_objects = [NWBHDF5IO(f, 'r') for f in probe_filenames]
-        for probe_io in io_objects:
-            probe_nwbfile = probe_io.read()
-            nwbfile = combine_probe_file_info(nwbfile, probe_nwbfile)
+        try:
+            for probe_io in io_objects:
+                probe_nwbfile = probe_io.read()
+                nwbfile = combine_probe_file_info(nwbfile, probe_nwbfile)
 
-        # change stimulus_template to Image objects in Images container
-        nwbfile = convert_stimulus_template_to_images(nwbfile)
+            # change stimulus_template to Image objects in Images container
+            nwbfile = convert_stimulus_template_to_images(nwbfile)
 
-        # add missing experiment description field (from technical white paper)
-        nwbfile = add_missing_descriptions(nwbfile)
-        
-        # export to zarr
-        with NWBZarrIO(zarr_filename, mode='w') as export_io:
-            export_io.export(src_io=read_io, nwbfile=nwbfile, write_args=dict(link_data=False))
+            # add missing experiment description field (from technical white paper)
+            nwbfile = add_missing_descriptions(nwbfile)
 
-        # close IO objects for probe files
-        for probe_io in io_objects:
-            probe_io.close()
+            # export to zarr
+            with NWBZarrIO(zarr_filename, mode='w') as export_io:
+                export_io.export(src_io=read_io, nwbfile=nwbfile, write_args=dict(link_data=False))
+        finally:
+            # close IO objects for probe files
+            for probe_io in io_objects:
+                probe_io.close()
     
     # inspect and validate the resulting zarr file
     inspect_zarr_file(zarr_filename, 
@@ -88,7 +89,7 @@ if __name__ == "__main__":
 
             # convert session hdf5_base_filename
             hdf5_base_filename = local_path / f"ecephys_session_{session_id}.nwb"
-            zarr_filename = f"./ecephys_session_{session_id}.nwb.zarr"
+            zarr_filename = Path(f"./ecephys_session_{session_id}.nwb.zarr")
             probe_filenames = [local_path / Path(f).name for f in hdf5_files if 'probe' in f]
 
             convert_visual_behavior_ephys_file_to_zarr(hdf5_base_filename, zarr_filename, probe_filenames)
@@ -114,5 +115,5 @@ if __name__ == "__main__":
                 b.fetch(base_filename, local_path / Path(base_filename).name)
 
             # convert session hdf5_base_filename
-            zarr_filename = f"./behavior_session_{session_id}.nwb.zarr"
-            convert_visual_behavior_ephys_file_to_zarr(local_path / Path(base_filename).name, Path(zarr_filename))
+            zarr_filename = Path(f"./behavior_session_{session_id}.nwb.zarr")
+            convert_visual_behavior_ephys_file_to_zarr(local_path / Path(base_filename).name, zarr_filename)
